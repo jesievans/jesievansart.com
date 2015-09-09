28d5f5be07e2b55a2d655613882b19a0de39f740
Jesi Evans <jesievans@gmail.com>
2015-09-09 00:15:19 -0400

    updates to CSS JS PHP media queries

--- js/work.js
+++ js/work.js
@@ -1,12 +1,50 @@
-//navigation
-//$('nav li ul').hide().removeClass('fallback');
-//$('nav li').onclick(
- // function () {
- //   $('ul', this).stop().slideDown(100);
- // },
- // function () {
- //   $('ul', this).stop().slideUp(100);
-//  }
-//);
+$(document).ready(function() {
 
-//$('.fallback').hide();
+  $(function() {
+    $('.dropdown').hover(function() {
+      $('.dropdown').css('background-color', '#555555');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.dropdown').css('background-color', '');
+    });
+  }); //end of dropdown hover
+
+  $(function() {
+    $('.flyout').hover(function() {
+      $('.flyout').css('background-color', '#4C4C4C');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.flyout').css('background-color', '');
+    });
+  }); //end of flyout hover
+
+  $(function() {
+    var form = $('#contactform');
+    var formMessages = $('#form-messages');
+    $(form).submit(function(event) {
+      event.preventDefault();
+      var formData = $(form).serialize();
+      $.ajax({
+          type: 'POST',
+          url: $(form).attr('action'),
+          data: formData
+      }).done(function(response) {
+          $(formMessages).removeClass('error');
+          $(formMessages).addClass('success');
+          $(formMessages).text(response);
+          $('#name').val('');
+          $('#email').val('');
+          $('#message').val('');
+      }).fail(function(data) {
+          $(formMessages).removeClass('success');
+          $(formMessages).addClass('error');
+          if (data.responseText !== '') {
+              $(formMessages).text(data.responseText);
+          } else {
+              $(formMessages).text('Oops! An error occured and your message could not be sent.');
+          }
+      }); //end of form submit
+    }); //end of event listener 
+  }); //end of form submit function
+
+}); //end of ready
